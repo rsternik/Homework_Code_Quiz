434c40b68a41a0c10c5b483cc368461631b436cc
rsternik <rsternik.wg@gmail.com>
2021-10-06 19:13:58 -0700

    some base opertations functioning. committing for iteration of project

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+// Selectors
+
+let mainEl = document.getElementById('quiz');
+let questionEl = document.getElementById('question');
+let choicesEl = document.getElementById('choices');
+
+
+//Creation elements
+let listContainer = document.createElement('ol');
+let list = document.createElement('li');
+let countDown = document.createElement('div');
+let headerEl = document.getElementById('top');
+ 
+// Vars
+let playerScore = 0;
+let secondsLeft = 10;
+
+// Arrays
+let questions = [
+    {
+        question: 'Commonly usd data types DO NOT include:',
+        choices: ['strings', 'boolean', 'alerts', 'numbers'],
+        correct: 'true'
+    },
+    {
+        question: 'The condition in an if / else statement is enclosed within ____',
+        choices: ['quotes', 'curly brackets', 'paratheses', 'square brackets'],
+        correct: 'true'
+    },
+    {
+        question: 'Arrays in JavaScript can be uses to store ____',
+        choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        correct: 'true'
+    },
+    {
+        question: 'String values must be enclosed within ____ when being assinged to variables.',
+        choices: ['commas', 'curly brackets', 'quotes', 'paratheses'],
+        correct: 'true'
+    },
+];
+
+
+// Countdown Timer
+
+function setTime() {
+    // Sets interval in variable
+    let headerEl = document.getElementById('top');
+    let countDown = document.createElement('div');
+    let timerInterval = setInterval(function() {
+        secondsLeft--;
+        countDown.textContent = secondsLeft;
+        headerEl.appendChild(countDown);
+        console.log(secondsLeft);
+        
+
+        if (secondsLeft === 0) {
+            // Kills Execution
+            clearInterval(timerInterval);
+            // Lose Screen
+            gameOver();
+        }
+
+    }, 1000);
+}
+
+// Game Over
+function gameOver() {
+}
+
+
+function renderQuestion(){
+
+questionEl.textContent = questions[0].question
+for (let i = 0; i < questions[0].choices.length; i++) {
+    let choice = document.createElement('button');
+    choice.innerHTML = questions[0].choices[i];
+    choicesEl.appendChild(listContainer).appendChild(choice);
+}
+
+}
+
+
+setTime();
+renderQuestion();
