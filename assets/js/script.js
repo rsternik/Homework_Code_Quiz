0dcf1fed6a7b22653144cd10e2d8b1b67919d1c8
rsternik <rsternik.wg@gmail.com>
2021-10-13 16:53:19 -0700

    Code cleaned up and quiz flox now in proper order. Scores not saved and reset nbutton not functional yet. This will eb completed in next commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,21 @@ let mainEl = document.getElementById('quiz');
 let questionEl = document.getElementById('question');
 let choicesEl = document.getElementById('choices');
 let headerEl = document.getElementById('top');
-let footerEl = document.getElementById('message')
+let messageEl = document.getElementById('message')
+let endGameEl = document.getElementById('endGame')
+let quizIntroEl = document.getElementById('quizIntro')
+let highScoresEl = document.getElementById('highScores')
 //Creation elements
 let listContainer = document.createElement('ol');
 let list = document.createElement('li');
 let countDown = document.createElement('div');
-
-
+let formInitials = document.createElement('input')
 // Vars
 let playerScore = 0;
 let playerName = 'Name:';
 let secondsLeft = 121;
 let wLeft = 1;
 let turn = 0;
-
 // Arrays
 let questions = [
     {
@@ -45,54 +46,20 @@ let questions = [
         correct: 'console.log'
     }
 ];
-
-
-// Countdown Timer
-function setTime() {
-    // Sets interval in variable
-    let headerEl = document.getElementById('top');
-    let countDown = document.createElement('div');
-    timerInterval = setInterval(function () {
-        secondsLeft--;
-        countDown.textContent = secondsLeft;
-        headerEl.appendChild(countDown);
-        if (secondsLeft < 0) {
-            // Kills Execution
-            clearInterval(timerInterval);
-            // Lose Screen
-            gameOver();
-        }
-    },1000);
-}
-
-// Game Over
-function gameOver() {
-    document.body.replaceChildren()
-    let finish = 'Game Over!'
-    console.log("Game over")
-}
-// Score Board
-function scoreBoard() {
-    document.body.replaceChildren()
-    console.log(secondsLeft)
-
-}
-
+// Display quiz questions and answers
 function renderQuestion() {
-    if (turn >= 5){
+    if (turn >= 5) {
         clearInterval(timerInterval)
         scoreBoard();
         return
-            }
-
+    }
     for (let i = 0; i < questions[i].choices.length; i++) {
 
         questionEl.textContent = questions[turn].question
         let choice = document.createElement('li');
         choice.textContent = questions[turn].choices[i];
         choicesEl.appendChild(listContainer).appendChild(choice)
-
-        // Records user choice
+        // User input
         choice.addEventListener('click', function () {
 
             choicesEl.removeChild(listContainer).remove
@@ -113,31 +80,100 @@ function renderQuestion() {
 
     }
 }
-
-function wrongAnswer(){   
-    footerEl.textContent = 'Incorrect';
-    document.body.appendChild(footerEl);
+// Display Correct or Incorrect answer message
+function wrongAnswer() {
+    messageEl.textContent = 'Incorrect';
+    document.body.appendChild(messageEl);
     let timerInterval = setInterval(function () {
         wLeft--;
         if (wLeft <= 0) {
             clearInterval(timerInterval);
-            footerEl.replaceChildren();        
+            messageEl.replaceChildren();
         }
 
     }, 500);
 }
-function rightAnswer(){    
-    footerEl.textContent = 'Correct';
-    document.body.appendChild(footerEl);
+function rightAnswer() {
+    messageEl.textContent = 'Correct';
+    document.body.appendChild(messageEl);
     let timerInterval = setInterval(function () {
         wLeft--;
         if (wLeft <= 0) {
             clearInterval(timerInterval);
-            footerEl.replaceChildren();            
+            messageEl.replaceChildren();
         }
 
     }, 500);
 }
+//End Game Score Board
+function scoreBoard() {
+    quizIntroEl.style.display = 'none'
+    mainEl.style.display ='none'
+    headerEl.style.display = 'none'
+    highScoresEl.style.display = 'block'
+    endGameEl.style.display = 'block'
+    console.log(secondsLeft)    
+    // Enter Initials prompt
+    let endMessage = document.createElement('p')
+    let initialsButton = document.createElement('button') 
+    let resetButton = document.createElement('button') 
+    endMessage.textContent = 'Enter your initials'
+    initialsButton.textContent = 'Submit'
+    resetButton.textContent = 'Reset Scores'
+    endGameEl.appendChild(endMessage)
+    endGameEl.appendChild(formInitials)
+    endGameEl.appendChild(initialsButton) 
+    endGameEl.appendChild(resetButton)
+    // Event Listeners
+    // Submit
+    initialsButton.addEventListener('click', function(){
 
-setTime();
-renderQuestion();
+    })
+    // Reset
+    resetButton.addEventListener('click', function(){
+
+    })
+
+}
+// Countdown Timer
+function setTime() {
+    // Sets interval in variable
+    let headerEl = document.getElementById('top');
+    let countDown = document.createElement('div');
+    timerInterval = setInterval(function () {
+        secondsLeft--;
+        countDown.textContent = secondsLeft;
+        headerEl.appendChild(countDown);
+        if (secondsLeft == 0) {
+            // Kills Execution
+            clearInterval(timerInterval);
+            // Scoreboard
+            scoreBoard();
+        }
+    },1000);
+}
+// Begin quiz
+function quizStart() {
+    quizIntroEl.style.display = 'none'
+    headerEl.style.display = 'flex'
+    mainEl.style.display ='block'
+    setTime()
+    renderQuestion()
+}
+// Quiz initialize
+function init() {
+    headerEl.style.display = 'none'
+    mainEl.style.display = 'none'
+    highScoresEl.style.display = 'none'
+    quizIntroEl.style.display = 'block'
+    console.log('click to start quiz')
+
+    let startButton = document.createElement('button')
+    startButton.textContent = 'Start'
+    quizIntroEl.appendChild(startButton)
+    startButton.addEventListener('click', function(){
+        quizStart()
+    })
+}
+init()
+
